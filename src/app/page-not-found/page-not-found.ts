936c268521e40ce7e25056fe5d6b6d8523d6f91e
saif101010 <W0857343@myscc.ca>
2025-10-31 17:22:42 -0400

    Completed Assignment 5

--- src/app/page-not-found/page-not-found.ts
+++ src/app/page-not-found/page-not-found.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './page-not-found.html',
-  styleUrl: './page-not-found.css',
+  styleUrl: './page-not-found.css'
 })
 export class PageNotFound {
 
+  constructor(private router: Router) {}
+
+  goHome(): void {
+    this.router.navigate(['/users']);
+  }
 }
