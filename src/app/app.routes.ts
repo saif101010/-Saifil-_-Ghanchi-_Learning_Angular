8709c9973d41717a17f141e4fef9d7433cbab574
saif101010 <W0857343@myscc.ca>
2025-11-15 14:44:43 -0500

    perf: Transition application to use lazy loading for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,32 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { UserListComponent } from './user-list/user-list';
-import { UserDetail } from './user-detail/user-detail';
-import { ModifyUser } from './modify-user/modify-user';
-import { PageNotFound } from './page-not-found/page-not-found';
+import { user-listComponent } from './user-list/user-list';
+// REMOVE imports for components you are about to lazy load
+// import { ModifyListItemComponent } from './modify-list-item/modify-list-item.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: 'users', component: UserListComponent },
-  { path: 'users/:id', component: UserDetail },
-  { path: 'modify', component: ModifyUser },
-  { path: 'modify/:id', component: ModifyUser },
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: '**', component: PageNotFound }
+  // Default route, redirects to the main student list
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+
+  // EAGER LOADED: This component's code loads on startup
+  { path: 'students', component: StudentListComponent },
+
+  // LAZY LOADED: This component's code only loads when a user navigates here
+  {
+    path: 'students/:id', // The detail view from Assignment 5 bonus
+    loadComponent: () => import('./student-detail/student-detail.component').then(m => m.StudentDetailComponent)
+  },
+
+  // LAZY LOADED: The modify component
+  {
+    path: 'modify-student/:id', // Assuming your route is like this
+    loadComponent: () => import('./modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent)
+  },
+
+  // LAZY LOADED: The page-not-found component
+  {
+    path: '**', // Wildcard route
+    loadComponent: () => import('./page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+  }
 ];
